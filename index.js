ae007f042ff6038d78e3970da0cbbe3859e2ea40
dhruv6-6 <94751267+dhruv6-6@users.noreply.github.com>
2023-06-06 10:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
 
-const {addUser , deleteUser , getUser , getUserById, getUserInRoom } = require('./users');
+const {addUser , deleteUser , getUser , getUserInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
     }
 
     socket.join(user.room);
-    
+
     socket.emit('message' , {user: 'admin' , text : `${user.name}, Welcome to ${user.room}`});
     socket.broadcast.to(user.room).emit('message' , {user: 'admin' , text : `${user.name} has joined`});
 
@@ -45,7 +45,6 @@ io.on('connection', (socket) => {
 
     io.to(user.room).emit('message' , {user : user.name , text: message});
     io.to(user.room).emit('roomData' , {room : user.room , text: message});
-
 
     callback();
   });
